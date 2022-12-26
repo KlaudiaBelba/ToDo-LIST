84d4dcddfaead4d23810649d7878e99a5c9395af
Klaudia <64747945+KlaudiaBelba@users.noreply.github.com>
2022-12-26 16:05:30 +0100

    Created a new To-Do List

---
+++ project.js
@@ -0,0 +1,58 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplate = (todo) => {
+
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span> ${todo} </span>
+      <i class="far fa-trash-alt delete"></i>
+  </li> 
+    `;
+    list.innerHTML += html;
+
+}
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  if(todo.length){
+  generateTemplate(todo);
+  addForm.reset();
+}
+
+});
+
+// delete todos
+
+list.addEventListener('click', e => {
+
+    if(e.target.classList.contains('delete')){
+       e.target.parentElement.remove();
+    }
+});
+
+//keyup
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterToDos(term);
+})
+
+const filterToDos = (term) => {
+   Array.from(list.children)
+      .filter((li) => {
+         return !li.textContent.toLowerCase().includes(term);
+      })
+      .forEach((li) => li.classList.add('filtered'));
+
+   Array.from(list.children)
+      .filter((li) => {
+         return li.textContent.toLowerCase().includes(term);
+      })
+      .forEach((li) => {
+        li.classList.remove('filtered')
+      });
+};
